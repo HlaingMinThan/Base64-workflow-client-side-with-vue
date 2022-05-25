e7f8c022a8089efebc2aad68a0eca26ed4cd0912
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-05-25 19:32:28 +0700

    sample workflow done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex justify-center items-center h-screen">
+    <div>
+      <h3 class="text-xl font-bold text-center mb-3">Base64 Preview Image</h3>
+      <div class="mx-10 rounded-md border w-72 h-64">
+        <img :src="image" alt="" class="object-contain w-72 h-64" v-if="image" />
+      </div>
+      <div class="flex justify-center mt-3">
+        <image-uploader @uploadedPreview="uploadedPreview" />
+      </div>
+    </div>
+    <div class="flex items-center">
+       <button
+          class="w-36 h-12 border rounded-md capitalize flex justify-center items-center gap-2"
+          @click="uploadToServer"
+          :class="{ 'bg-green-400 text-white' :!!uploadedImage}"
+          :disabled="!!uploadedImage"
+        >
+          {{uploadedImage ? "Uploaded": "Upload" }} to server
+        </button>
+    </div>
+    <div>
+      <h3 class="text-xl font-bold text-center mb-3">response image</h3>
+      <div class="mx-10 rounded-md border w-72 h-64">
+        <img :src="responseImage" alt="" class="object-contain w-72 h-64" v-if="responseImage"/>
+      </div>
+      <div class="flex justify-center mt-3">
+        <button
+          class="w-36 h-12 border rounded-md capitalize flex justify-center items-center gap-2"
+          @click="retreiveImageFromServer"
+          :class="{ 'bg-green-400 text-white' :!!responseImage}"
+          :disabled="!!responseImage"
+        >
+          {{responseImage ? "Retrieved": "Retrieve" }} image from server
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ImageUploader from "./components/ImageUploader.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ImageUploader,
+  },
+  inject: ["$axios"],
+  data() {
+    return {
+      uploadedImage:"",
+      image: "",
+      responseImage: "",
+    };
+  },
+  methods: {
+    async uploadedPreview(base64) {
+      this.image = base64; //preview base64 image on client side
+    },
+    async uploadToServer() {
+      const res = await this.$axios.post("/api/upload", {
+        base64: this.image,
+      });
+      this.uploadedImage=res.data//store the response server image
+    },
+    retreiveImageFromServer(){
+      this.responseImage=this.uploadedImage//show the retrieve Image on client
+    }
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/ImageUploader.vue
+++ src/components/ImageUploader.vue
@@ -9,22 +9,28 @@
       @change="uploadPreviewImage"
     />
     <button
-      className="w-32 h-12 border rounded-md capitalize flex justify-center items-center gap-2"
+      class="w-40 h-12 py-7 border rounded-md capitalize flex justify-center items-center gap-2"
       @click="openUploader"
+      :class="{ 'bg-green-400 text-white': uploaded }"
+      :disabled="uploaded"
     >
-      Upload Image
+      {{uploaded?'Uploaded':'Upload'}} Image As Base64
     </button>
   </div>
 </template>
 
 <script>
-
 export default {
   props: {
     accept: {
       type: String,
       default: "",
     },
+  },
+  data() {
+    return {
+      uploaded: false,
+    };
   },
   methods: {
     openUploader() {
@@ -34,6 +40,7 @@ export default {
       const reader = new FileReader();
       reader.addEventListener("load", () => {
         this.$emit("uploadedPreview", reader.result);
+        this.uploaded = true;
       });
       reader.readAsDataURL(this.$refs.uploader.files[0]);
     },
